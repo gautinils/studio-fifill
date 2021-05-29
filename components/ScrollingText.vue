ffe7e735db6d9b961b75f4f32bc20a0ecabdcfb3
Gauti Nils Bernhardsson <gautinils@gmail.com>
2021-05-29 17:53:14 +0000

    Lets work together srolling text

---
+++ components/ScrollingText.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="scroll">
+    <span class="text-5xl">{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.scroll {
+  overflow: hidden;
+  position: relative;
+  height: 50px;
+}
+span {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  line-height: 50px;
+
+  /* Starting position */
+  -moz-transform: translateX(100%);
+  -webkit-transform: translateX(100%);
+  transform: translateX(100%);
+
+  /* Apply animation to this element */
+  -moz-animation: scroll-left 10s linear infinite;
+  -webkit-animation: scroll-left 10s linear infinite;
+  animation: scroll-left 10s linear infinite;
+}
+
+/* Move it (define the animation) */
+@-moz-keyframes scroll-left {
+  0% {
+    -moz-transform: translateX(100%);
+  }
+  100% {
+    -moz-transform: translateX(-30%);
+  }
+}
+@-webkit-keyframes scroll-left {
+  0% {
+    -webkit-transform: translateX(100%);
+  }
+  100% {
+    -webkit-transform: translateX(-30%);
+  }
+}
+@keyframes scroll-left {
+  0% {
+    -moz-transform: translateX(100%); /* Browser bug fix */
+    -webkit-transform: translateX(100%); /* Browser bug fix */
+    transform: translateX(100%);
+  }
+  100% {
+    -moz-transform: translateX(-30%); /* Browser bug fix */
+    -webkit-transform: translateX(-30%); /* Browser bug fix */
+    transform: translateX(-30%);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,87 @@
 <template>
-  <div class="container" :class="{ black }">
-    <h1
-      class="text-9xl font-medium"
-      @mouseover="() => (black = true)"
-      @mouseout="() => (black = false)"
-    >
-      Stúdíó fífill
-    </h1>
-    <div class="text p-5">
-      <p>Design studio based in</p>
-      <p>Brussels & Reykjavík</p>
-      <p>elsa@fifill.co</p>
-      <p>gauti@fifill.co</p>
-    </div>
+  <div class="container" :class="{ black }" @scroll="handleScroll">
+    <section class="hero">
+      <h1 class="text-9xl font-medium">
+        <span
+          @mouseover="() => (black = true)"
+          @mouseout="() => (black = false)"
+        >
+          Stúdíó fífill
+        </span>
+      </h1>
+      <div class="work-with-us" :class="!black ? 'invisible' : ''">
+        <scrolling-text text="Let's work together" />
+      </div>
+    </section>
+
+    <section class="text text-xl p-5" :class="{ visible: showText }">
+      <div>
+        <p>Design studio based in</p>
+        <p>Brussels & Reykjavík</p>
+      </div>
+      <div class="pl-20">
+        <p>elsa@fifill.co</p>
+        <p>gauti@fifill.co</p>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import ScrollingText from '~/components/ScrollingText.vue'
 export default {
+  components: { ScrollingText },
   data() {
     return {
       black: false,
+      showText: false,
     }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll(event) {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        this.showText = true
+        window.removeEventListener('scroll', this.handleScroll)
+      }
+    },
   },
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  justify-content: space-between;
   display: flex;
   flex-direction: column;
   background: linear-gradient(#f0eb26, #fff);
   min-width: 100vw;
 }
+.hero {
+  min-height: 100vh;
+}
 .black {
   background: black;
+}
+.text {
+  transform: translateX(-500px);
+  opacity: 0;
+  transition: all 0.95s cubic-bezier(0.475, 0.425, 0, 0.99) 0s;
+  display: flex;
+  &.visible {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+.work-with-us {
+  color: #f0eb26;
+  font-family: antique-olive-compact, sans-serif;
+  font-style: italic;
 }
 h1 {
   color: #f0eb26;
@@ -46,6 +90,8 @@ h1 {
   -webkit-text-stroke: 1px black;
   font-family: antique-olive-compact, sans-serif;
   font-style: italic;
-  cursor: pointer;
+  span {
+    cursor: pointer;
+  }
 }
 </style>
